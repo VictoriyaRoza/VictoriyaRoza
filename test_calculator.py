be0b7936712f3122b95f2837ce1217fb2176d9e3
victoria.tomashevska <victoria.tomashevska@yandex.ru>
2024-08-01 14:48:01 +0300

    TP

---
+++ test_calculator.py
@@ -0,0 +1,10 @@
+from calculator import Calculator
+
+# Задание
+# Разработчику 1 - реализовать функцию умножения
+# Разработчику 2 - реализовать функцию деления
+
+
+calc = Calculator()
+calc.sum(10, 5)
+calc.print_last_res()
